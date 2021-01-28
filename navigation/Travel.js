990bea2d527ba4fa589c9753a30af7d7ff5ec9e4
malay3 <malay@goodhealthapp.com>
2021-01-28 22:35:32 +0530

    first commit

---
+++ navigation/Travel.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
+
+import List from "./../screens/List";
+import Article from "./../screens/Article";
+
+export default createStackNavigator(
+    {
+        List,
+        Article
+    },
+    //     {
+    // headerMode:false
+    //     },
+    {
+        initialRouteName: "List"
+    }
+);
